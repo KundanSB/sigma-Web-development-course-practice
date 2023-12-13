560e2b03e77fc35b902e60130eff72add89513eb
Bhoye Kundan Santosh <91618522+KundanSB@users.noreply.github.com>
2023-12-13 21:09:26 +0530

    todays

---
+++ V57-loops/index.js
@@ -0,0 +1,42 @@
+console.log("This is all about Loops");
+
+//Normal printing
+// let a=1;
+// console.log(a);
+// console.log(a+1);
+// console.log(a+2);
+
+let a = 1;
+// console.log(a)
+// console.log(a+1)
+// console.log(a+2)
+
+// for (let i = 0; i < 100; i++) { 
+//     console.log(a + i); 
+// }
+
+// let obj = {
+//     name: "Harry",
+//     role: "Programmer",
+//     company: "CodeWithHarry AI"
+// }
+
+// for (const key in obj) {  
+//         console.log(key)
+// }
+
+// for (const c of "Harry") {
+//     console.log(c)
+// }
+
+// let i = 0;
+// while (i<60000) {
+//     console.log(i)
+//     i++;
+// }
+
+let i = 10;
+do {
+    console.log(i)
+    i++;
+} while (i < 6);
